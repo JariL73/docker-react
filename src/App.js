2edc550fa8a20103d528bc796480f4895db2e7e7
jarilah <jari.lahdenpera@solita.fi>
2019-01-10 09:49:22 +0200

    Minor updates to tests and .js file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edited <code>src/App.js</code> and reloaded 3.
+            Elastic Beanstalk Rocks!
           </p>
           <a
             className="App-link"
